c51604678248db4760ac391d5e8730619e3dee62
Danyaal Patel <danyaalp@sfu.ca>
2021-03-13 13:34:09 -0800

    Have basic methods and attributes for Game class

--- src/main/java/Core/Game.java
+++ src/main/java/Core/Game.java
@@ -1,4 +1,44 @@
 package Core;
+import javafx.application.Application;
+import javafx.stage.Stage;
 
-public class Game {
+
+import java.util.Locale;
+
+public class Game{
+
+    private int score;
+    private int time;
+
+    public void startGame(){
+        score = 0;
+        time = 0;
+    }
+
+    public void endGame(){
+
+    }
+    public int getScore(){
+        return score;
+    }
+
+    public int getTime(){
+        return time;
+    }
+
+    public int changeScore(int amount){
+        score = score + amount;
+        return score;
+    }
+
+
+    public static void main(String[] args) {
+        Game game = new Game();
+        int testscore = game.getScore();
+        System.out.println(testscore);
+        game.changeScore(23);
+        int testscore2 = game.getScore();
+        System.out.println(testscore2);
+    }
+
 }
