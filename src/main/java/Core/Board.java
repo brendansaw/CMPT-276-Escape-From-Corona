92119189ef816dac89acdee92dcb0603b3e2a9c2
PeterSalnikov <psalniko@sfu.ca>
2021-03-12 21:00:51 -0800

    added more cases, beginning to implement class Tile

--- src/main/java/Core/Board.java
+++ src/main/java/Core/Board.java
@@ -18,13 +18,16 @@ public class Board
     Not sure if these are necessary based on tilemap implementation
     */
 
-    public Board()
+    public Board(int[][] id)
     {
 
-        /**Reads a file with a 2D array...
+        /**Takes a 2D array...
      *... and initializes each co-ordinate to a Tile object determined by the id at each entry
      *
      */
+
+        boardID = id;
+
         int cur;
         for(int i = 0; i < dim; i++)
         {
@@ -39,20 +42,26 @@ public class Board
                     case 1://Barrier
                         board[i][j] = new Wall();
                     case 2://Tile with checkpoint
-                        board[i][j] = new Reward();
+                        //board[i][j] = new Reward(/*checkpoint params*/);
                     case 3://Tile with punishment
-                        board[i][j] = new Reward();
-
-                    case 4://Tile with bonus reward. Perhaps the location   
-                        board[i][j] = new Reward();
+                        //board[i][j] = new Reward(/*punishment params*/);
+                    case 4://Tile with bonus reward. Perhaps the location is predetermined since it is here, to group it with all the other rewards
+                        //board[i][j] = new Reward(/*bonus params*/);
+                    case 5://Tile with entry
+                        entryPos = new int[]{i,j};
+                    case 6://Tile with exit. Cannot be accessed until all checkpoints collected!
+                        exitPos = new int[]{i,j};
 
                 }
             }
         }
-
-
     }
 
+    public void setID(int i, int j, int val)
+    {
+        //Changes the ID of a single entry, like for when a reward is stepped on
+        boardID[i][j] = val;
+    }
 }
 
 
--- src/main/java/Core/Tile.java
+++ src/main/java/Core/Tile.java
@@ -4,9 +4,21 @@ public class Tile
 {
         Boolean hasEnemy;
         Boolean hasMain;
+        Boolean hasReward;
+
     public Tile()
     {
 
+    }
+
+    public Boolean getHasEnemy()
+    {
+        return hasEnemy;
+    }
+
+    public Boolean getHasMain()
+    {
+        return hasMain;
     }
 
 }
