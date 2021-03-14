81c71eeed3a47c469fb311540d82080eb376091f
PeterSalnikov <psalniko@sfu.ca>
2021-03-13 20:21:51 -0800

    Working on implementation of tile grid. Next step is to display a different looking tile depending on the id on its co-ordinates.

--- src/main/java/Core/Game.java
+++ src/main/java/Core/Game.java
@@ -6,12 +6,16 @@ import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.chart.BarChart;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
+import javafx.scene.shape.*;
 
 
+import java.awt.*;
 import java.util.Locale;
 
 public class Game extends Application{
@@ -32,25 +36,59 @@ public class Game extends Application{
         int width = boardGame.getDimX();
         int height = boardGame.getDimY();
 
-        Group root = new Group();
-        Scene theScene = new Scene(root);
-        mainGame.setScene(theScene);
+//        Group root = new Group();
+//        Scene theScene = new Scene(root);
+//        mainGame.setScene(theScene);
 
-        Canvas canvas = new Canvas(100 + width*100 , 100 + height*100);
-        root.getChildren().add( canvas);
+//        Canvas canvas = new Canvas(100 + width*100 , 100 + height*100);
+//        root.getChildren().add( canvas);
+
+//        Rectangle[][] rect = new Rectangle[height][width];
+//
+//        for(int i = 0; i < height; i++){
+//            for(int j = 0; j < width; j++){
+//                rect[i][j] = new Rectangle();
+//            }
+//        }
+
+        AnchorPane root = new AnchorPane();
+        Scene scene = new Scene(root);
+        mainGame.setScene(scene);
+
+        int horizontal = 256, vertical = 256;
+        Rectangle rect = null;
+        for(int i = 0; i < width; ++i)
+        {//Iterate through columns
+            for(int j = 0; j < height; ++j)
+            {//Iterate through rows
+//              Color choice = chooseColor(rectColors);
+                //Method that chooses a color
+
+                rect = new Rectangle(horizontal*j, vertical*i, horizontal, vertical);
+                //Create a new rectangle(PosY,PosX,width,height)
 
 
+                rect.setStroke(Color.RED);
+                //Give rectangles an outline so I can see rectangles
 
-        GraphicsContext gc = canvas.getGraphicsContext2D();
-        gc.setFill( Color.GREEN );
-        gc.setStroke( Color.GREEN );
-        gc.setLineWidth(2);
-        Font theFont = Font.font( "Times New Roman", FontWeight.BOLD, 48 );
-        gc.setFont( theFont );
-        gc.fillText( "Start Game", 60, 50 );
-        gc.strokeText( "Start Game", 60, 50 );
+                root.getChildren().add(rect);
+                //Add Rectangle to board
 
+            }
+        }
+        scene.setRoot(root);
         mainGame.show();
+
+
+//        GraphicsContext gc = canvas.getGraphicsContext2D();
+//        gc.setFill( Color.GREEN );
+//        gc.setStroke( Color.GREEN );
+//        gc.setLineWidth(2);
+//        Font theFont = Font.font( "Times New Roman", FontWeight.BOLD, 48 );
+//        gc.setFont( theFont );
+//        gc.fillText( "Start Game", 60, 50 );
+//        gc.strokeText( "Start Game", 60, 50 );
+
 
     }
 
