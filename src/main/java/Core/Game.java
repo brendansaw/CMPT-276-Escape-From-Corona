b18d1858248f8d3153e8eabb5a09f8fc8efc2e2d
Danyaal Patel <danyaalp@sfu.ca>
2021-03-13 20:23:05 -0800

    Java FX for Maze implemented with rectangles. Basic version

--- src/main/java/Core/Game.java
+++ src/main/java/Core/Game.java
@@ -6,7 +6,9 @@ import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.chart.BarChart;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
@@ -39,6 +41,45 @@ public class Game extends Application{
         Canvas canvas = new Canvas(100 + width*100 , 100 + height*100);
         root.getChildren().add( canvas);
 
+ /*       Rectangle rect = new Rectangle();
+
+        for (int i = 0; i < boardGame.getDimY(); i++){
+            for (int j = 0; j < boardGame.getDimX(); j++){
+                Rectangle rect = new Rectangle();
+                rect.setX(150.0f);
+                rect.setY(75.0f);
+                rect.setWidth(300.0f);
+                rect.setHeight(150.0f);
+            }
+        }
+*/
+        AnchorPane rectroot = new AnchorPane();
+        Scene scene = new Scene(rectroot);
+        mainGame.setScene(scene);
+
+        int columns = height, rows = width, horizontal = 100, vertical = 100;
+        Rectangle rect = null;
+        for(int i = 1; i <= columns; ++i)
+        {//Iterate through columns
+            for(int j = 1; j <= rows; ++j)
+            {//Iterate through rows
+//              Color choice = chooseColor(rectColors);
+                //Method that chooses a color
+
+                rect = new Rectangle(horizontal*j, vertical*i, horizontal, vertical);
+                //Create a new rectangle(PosY,PosX,width,height)
+
+                rect.setStroke(Color.RED);
+                //Give rectangles an outline so I can see rectangles
+
+                rectroot.getChildren().add(rect);
+                //Add Rectangle to board
+
+            }
+        }
+        scene.setRoot(rectroot);
+        mainGame.show();
+        //Rectangle rectangle = new Rectangle(x, y, width, height)
 
 
         GraphicsContext gc = canvas.getGraphicsContext2D();
@@ -47,8 +88,8 @@ public class Game extends Application{
         gc.setLineWidth(2);
         Font theFont = Font.font( "Times New Roman", FontWeight.BOLD, 48 );
         gc.setFont( theFont );
-        gc.fillText( "Start Game", 60, 50 );
-        gc.strokeText( "Start Game", 60, 50 );
+        gc.fillText( "MAZE GAME", 50, 50 );
+        gc.strokeText( "MAZE GAME", 50, 50 );
 
         mainGame.show();
 
@@ -91,6 +132,7 @@ public class Game extends Application{
 
     public static void main(String[] args) {
         launch(args);
+
         Game game = new Game();
         int testscore = game.getScore();
         System.out.println(testscore);
