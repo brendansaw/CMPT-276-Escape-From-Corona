4d9c5ef9ac5c25520d234d714a7b18f26694836f
sdao <sdao@sfu.ca>
2021-04-08 00:31:08 -0700

    Add tests for Enemy checkBestMovement method

--- src/test/java/Characters/EnemyTest.java
+++ src/test/java/Characters/EnemyTest.java
@@ -59,65 +59,92 @@ public class EnemyTest {
         assertEquals(1, enemy.getY(), "y coordinate should not change");
     }
 
-//    @Test
-//    public void testMoveTowardsPlayerX() {
-//        Enemy enemySpy = spy(enemy);
-//
-//        ArrayList<Enemy> enemyArrayList = new ArrayList<>();
-//        enemyArrayList.add(enemy);
-//
-//        mainCharacter.setPos(0, 0);
-//
-//        doNothing().when(enemySpy).onPlayerEntered();
-//
-//        int[][] testId = {
-//                {0, 1, 0},
-//                {0, 0, 0},
-//                {1, 1, 0}
-//        };
-//        board.setBoard(testId);
-//
-//        enemy.move(enemyArrayList);
-//        assertEquals(0, enemy.getX(), "x coordinate should decrease by 1");
-//        assertEquals(1, enemy.getY(), "y coordinate should not change");
-//    }
-//
-//    @Test
-//    public void testMoveTowardsPlayerY() {
-//        Enemy enemySpy = spy(enemy);
-//
-//        ArrayList<Enemy> enemyArrayList = new ArrayList<>();
-//        enemyArrayList.add(enemy);
-//
-//        mainCharacter.setPos(2, 2);
-//
-//        doNothing().when(enemySpy).onPlayerEntered();
-//
-//        int[][] testId = {
-//                {0, 1, 0},
-//                {0, 0, 1},
-//                {1, 0, 0}
-//        };
-//        board.setBoard(testId);
-//
-//        enemy.move(enemyArrayList);
-//        assertEquals(1, enemy.getX(), "x coordinate should not change");
-//        assertEquals(2, enemy.getY(), "y coordinate should increase by 1");
-//    }
-//
-//    @Test
-//    public void testMoveOntoPlayer() {
-//        Enemy enemySpy = spy(enemy);
-//
-//        ArrayList<Enemy> enemyArrayList = new ArrayList<>();
-//        enemyArrayList.add(enemy);
-//
-//        mainCharacter.setPos(0, 0);
-//
-//        doNothing().when(enemySpy).onPlayerEntered();
-//
-//        enemy.move(enemyArrayList);
-//        assertEquals(0, enemy.getX(), "x coordinate should decrease by 1");
-//        assertEquals(1, enemy.getY(), "y coordinate should not change");
-//    }
+    @Test
+    public void testCheckBestMovementUp() {
+        ArrayList<Enemy> enemyArrayList = new ArrayList<>();
+        enemyArrayList.add(enemy);
+
+        mainCharacter.setPos(0, 0);
+
+        int[][] testId = {
+                {0, 0, 0},
+                {1, 0, 0},
+                {1, 1, 0}
+        };
+        board.setBoard(testId);
+
+        assertEquals(Direction.UP, enemy.checkBestMovement(enemyArrayList), "should return Direction.UP");
+    }
+
+    @Test
+    public void testCheckBestMovementDown() {
+        ArrayList<Enemy> enemyArrayList = new ArrayList<>();
+        enemyArrayList.add(enemy);
+
+        mainCharacter.setPos(2, 2);
+
+        int[][] testId = {
+                {0, 0, 0},
+                {0, 0, 1},
+                {0, 0, 0}
+        };
+        board.setBoard(testId);
+
+        assertEquals(Direction.DOWN, enemy.checkBestMovement(enemyArrayList), "should return Direction.DOWN");
+    }
+
+    @Test
+    public void testCheckBestMovementLeft() {
+        ArrayList<Enemy> enemyArrayList = new ArrayList<>();
+        enemyArrayList.add(enemy);
+
+        mainCharacter.setPos(0, 0);
+
+        int[][] testId = {
+                {0, 0, 0},
+                {0, 0, 0},
+                {0, 0, 0}
+        };
+        board.setBoard(testId);
+
+        assertEquals(Direction.LEFT, enemy.checkBestMovement(enemyArrayList), "should return Direction.LEFT");
+    }
+
+    @Test
+    public void testCheckBestMovementRight() {
+        ArrayList<Enemy> enemyArrayList = new ArrayList<>();
+        enemyArrayList.add(enemy);
+
+        mainCharacter.setPos(2, 2);
+
+        int[][] testId = {
+                {0, 0, 0},
+                {0, 0, 0},
+                {0, 0, 0}
+        };
+        board.setBoard(testId);
+
+        assertEquals(Direction.RIGHT, enemy.checkBestMovement(enemyArrayList), "should return Direction.RIGHT");
+    }
+
+    @Test
+    public void testCheckBestMovementUpEnemyCollide() {
+        ArrayList<Enemy> enemyArrayList = new ArrayList<>();
+        enemyArrayList.add(enemy);
+
+        Enemy enemy2 = new Enemy(0, 1);
+        enemy2.setPos(0, 1);
+        enemyArrayList.add(enemy2);
+
+        mainCharacter.setPos(0, 0);
+
+        int[][] testId = {
+                {0, 1, 1},
+                {0, 0, 1},
+                {1, 1, 1}
+        };
+        board.setBoard(testId);
+
+        assertEquals(Direction.STAY, enemy.checkBestMovement(enemyArrayList), "should return Direction.STAY");
+    }
 }
