93886a14639902c19cd06ca9ff21bff7ed0494bf
sdao <sdao@sfu.ca>
2021-04-08 00:45:17 -0700

    Add test for NonStationaryCharacter setPos

--- src/test/java/Characters/NonStationaryCharacterTest.java
+++ src/test/java/Characters/NonStationaryCharacterTest.java
@@ -1,5 +1,15 @@
 package Characters;
+
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class NonStationaryCharacterTest {
+    NonStationaryCharacter character = new NonStationaryCharacter();
+
+    @Test
+    public void testSetPos() {
+        character.setPos(12, 34);
+        assertEquals(12, character.getX(), "x should be set to 12");
+        assertEquals(34, character.getY(), "y should be set to 34");
+    }
 }
